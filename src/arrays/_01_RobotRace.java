c329217b4814fb6fb1edb22c67e08ea514ebc66a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-12-04 16:59:59 -0800

    A new correct Robot Race using an array

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -7,26 +7,24 @@ import org.jointheleague.graphical.robot.Robot;
 public class _01_RobotRace {
 	//1. make a main method
 public static void main(String[] args) {
-Robot robby = new Robot();
-Robot robert = new Robot();
-Robot rob = new Robot();
-Robot bob = new Robot();
-Robot bobby = new Robot();
-Robot[] robots = {robby, robert, rob, bob, bobby};
+Robot.setWindowSize(600, 500);
+
+Robot [] robots = new Robot[5];
 
 	//2. create an array of 5 robots.
-
-	//3. use a for loop to initialize the robots.
+for (int i = 0; i < robots.length; i++) {
+	robots[i]=new Robot();
+	robots[i].moveTo(400+i*100, 400);
+}
+	//3. use a for loop to initialize the robots.ob
 
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
-for (int i=0;i<robots.length;i++) {
-    Random r = new Random(); 
-    robots[i].move(r.nextInt(50));
-}
-}
+//Code below is for the random movement and while loop
+//Random r = new Random();
+//robots[i].move(r.nextInt(50));
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 	//7. declare that robot the winner and throw it a party!
@@ -34,4 +32,5 @@ for (int i=0;i<robots.length;i++) {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
