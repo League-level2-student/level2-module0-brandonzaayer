1fe6959dba6d95b020f29be15f7ba880fc6fbf51
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-12-18 16:57:47 -0800

    Button stuff idk what it does yet i havent run it

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -15,14 +15,16 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class _02_FindTheHiddenButton implements ActionListener{
 	JFrame window;
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton [] b = new JButton[5];
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
@@ -33,16 +35,24 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String answer = JOptionPane.showInputDialog("Can I get a number?");
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		int bSize = Integer.parseInt(answer);
+		JPanel p = new JPanel();
+		for (int i = 0; i < b.length; i++) {
+			b[i]= new JButton();
+			p.add(b[i]);
+			b[i].addActionListener(this);
+			b[i].setSize(bSize, bSize);
+		}
 		//5. Make a for loop to iterate through the JButton array
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
 		
-		//9 add the panel to the window
-		
+		//9 add the panel to the frame
+		JFrame f = new JFrame();
+		f.add(p);
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
 		
 		//11. set the JFrame to visible.
